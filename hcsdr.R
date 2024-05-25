3cd9bd46ffb2c8650d346aacff224f487c9b59f0
sbhong95 <sbhong95@korea.ac.kr>
2024-05-25 20:48:46 +0900

    Update hcsdr.R
    update

--- hcsdr.R
+++ hcsdr.R
@@ -3,9 +3,12 @@ require(HellCor)
 require(SphereOptimize)
 require(dr)
 require(MAVE)
-require(beepr)
 require(doParallel)
+require(Ball)
+require(dHSIC)
 require(energy)
+require(Rdonlp2)
+
 
 ## Hellinger Correlation 
 HCXY <- function(beta, X, Y){
@@ -106,16 +109,8 @@ drr <- function(x, y, h, r){
   return(signrt %*% eigen(out)$vectors[, 1:r])
 }
 
-iht <- function(x, y, r){
-  z <- standmat(x)
-  szy <- cov(z, y)
-  szz <- var(z)
-  p <- dim(z)[2]
-  imat <- szy
-  for(i in 1:(p - 1)){
-    imat <- cbind(imat, matpower(szz,i) %*% szy)
-  }
-  return(eigen(imat %*% t(imat))$vectors[, 1:r])
+diff_measure <- function(vec1, vec2){
+  return(first_eigen(proj_matrix(vec1) - proj_matrix(vec2)))
 }
 
 ## optimize
@@ -157,3 +152,118 @@ hcsdr = function(x, y, SDRmethod="DR", maxit = 10000){
   hcdr = sphoptim(hcdr2, HCXY, X = x, Y = y,control = list(maxit = 10000))$par
   return(hcdr)
 }
+
+##########################
+####  Modern SDR part ####
+##########################
+
+nlcon = function(vec) as.numeric(vec%*%vec)
+dnlcon = function(vec) 2 * vec
+attr(nlcon, "gr") =  dnlcon
+nlin.l = nlin.u = 1
+
+#### Initial value for optimization for each dependency measure
+init_value = function(x, y, SDRmethod = "DCOV", n = 200){
+  p = dim(x)[2]
+  X <<- x; Y <<- y
+  value = 0
+  drout = rep(0, p)
+  for(i in 1:n){
+    temp_dr = normalize(rnorm(p))
+    temp_value = switch(SDRmethod,
+                   "DCOV" = dcov_ftn(temp_dr),
+                   "HSIC" = HSIC_ftn(temp_dr),
+                   "BCOV" = bcov_ftn(temp_dr),
+                   "method error")
+    if(temp_value < value){
+      value = temp_value
+      drout = temp_dr
+    }
+  }
+  return(drout)
+}
+
+#### DCOV part
+dcov_ftn = function(init){
+  xb = as.vector(X%*%init)
+  y = as.vector(Y)
+  return(-dcov(xb, y))
+}
+
+sdr_via_dcov = function(init = NULL, X, Y){
+  if (is.null(init)){
+    init = init_value(X, Y, "DCOV")
+  }
+  X <<- X; Y <<- Y
+  p = dim(X)[2]
+  par.l = rep(-1, p)
+  par.u = rep(1, p)
+  res = donlp2(init, dcov_ftn, par.l = par.l, par.u = par.u, nlin =list(nlcon), nlin.u = nlin.u, nlin.l = nlin.l)
+  return(res$par)
+}
+
+#### HSIC part
+HSIC_ftn = function(init){
+  xb = as.vector(X%*%init)
+  y = as.vector(Y)
+  return(-dhsic(xb, y)$dHSIC)
+}
+
+sdr_via_HSIC = function(init = NULL, X, Y){
+  if (is.null(init)){
+    init = init_value(X, Y, "HSIC")
+  }
+  X <<- X; Y <<- Y
+  p = dim(X)[2]
+  par.l = rep(-1, p)
+  par.u = rep(1, p)
+  res = donlp2(init, HSIC_ftn, par.l = par.l, par.u = par.u, nlin =list(nlcon), nlin.u = nlin.u, nlin.l = nlin.l)
+  return(res$par)
+}
+
+#### BCOV part
+bcov_ftn = function(init){
+  xb = as.vector(X%*%init)
+  y  = as.vector(Y)
+  return(-bcov(xb, y)) 
+}
+
+
+sdr_via_bcov = function(init = NULL, X, Y){
+  if (is.null(init)){
+    init = init_value(X, Y, "BCOV")
+  }
+  X <<- X; Y <<- Y
+  p = dim(X)[2]
+  par.l = rep(-1, p)
+  par.u = rep(1, p)
+  res = donlp2(init, bcov_ftn, par.l = par.l, par.u = par.u, nlin=list(nlcon), nlin.u=nlin.u, nlin.l=nlin.l)
+  return(res$par)
+}
+
+hcsdr_modern = function(x, y, SDRmethod="DCOV", maxit = 10000){
+  
+  f = function(beta) HCXY(beta,X = x,Y = y)
+  p = dim(x)[2]
+  
+  #### Optimization Constraint for Rdonlp2 package
+  nlcon = function(vec) as.numeric(vec%*%vec)
+  dnlcon = function(vec) 2 * vec
+  attr(nlcon, "gr") =  dnlcon
+  nlin.l = nlin.u = 1
+  par.l = rep(-1, p)
+  par.u = rep(1, p)
+  
+  drout <- switch(SDRmethod,
+                  "DCOV" = sdr_via_dcov(X = x, Y = y),
+                  "HSIC" = sdr_via_HSIC(X = x, Y = y),
+                  "BCOV" = sdr_via_bcov(X = x, Y = y),
+                  "method error")
+  init <- normalize(drout)
+  
+  #### HCOR part
+  hcdr2 = SphereOptimize(init, f, method="SANN", control = list(maxit = maxit*2))$par
+  hcdr = sphoptim(hcdr2, HCXY, X = x, Y = y,control = list(maxit = 10000))$par
+  return(hcdr)
+}
+
